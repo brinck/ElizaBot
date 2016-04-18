8d6c5c9d59fe738e68f470c44292dbb0bc85c2b1
Frederik Jensen <brinck10@gmail.com>
2016-04-18 22:07:35 +0400

    Added verification for fb

--- server.go
+++ server.go
@@ -19,8 +19,8 @@ func init() {
     http.HandleFunc("/webhook/", eliza)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Hello, world!")
+func handler(wr http.ResponseWriter, req *http.Request) {
+    fmt.Fprint(wr, "Hello, world!")
 }
 
 
@@ -68,6 +68,14 @@ type Message struct {
  * with the elizabot.
  */
 func eliza(wr http.ResponseWriter, req *http.Request) {
+
+	// verify facebook validation token
+	token := req.URL.Query().Get("hub.verify_token")
+	if (token == "quanfucius") {
+		fmt.Fprint(wr, req.URL.Query().Get("hub.challenge"))
+	}
+
+
 	// parse the request in json format
 	var data Webhook
 	dec := json.NewDecoder(req.Body)
